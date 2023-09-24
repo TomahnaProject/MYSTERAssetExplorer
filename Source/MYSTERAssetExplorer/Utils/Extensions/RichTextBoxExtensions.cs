dc054b05d2829ce24804d556c85b556fcd68d109
James Thomas <julyfortoda@gmail.com>
2023-09-24 12:18:22 -0400

    Separated out utility extensions methods more cleanly

---
+++ Source/MYSTERAssetExplorer/Utils/Extensions/RichTextBoxExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MYSTERAssetExplorer.Utils
+{
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+
+            box.SelectionStart = box.Text.Length;
+            box.ScrollToCaret();
+        }
+    }
+}
+
---
+++ Source/MYSTERAssetExplorer/Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MYSTERAssetExplorer.Utils
+{
+    public static class StringExtensions
+    {
+        public static bool CaseInsensitiveContains(this string text, string value,
+            StringComparison stringComparison = StringComparison.CurrentCultureIgnoreCase)
+        {
+            return text.IndexOf(value, stringComparison) >= 0;
+        }
+    }
+}
+
